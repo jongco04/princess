5c4a1182b98cc4a75003658911fdd5550e926d91
jongco04 <princessjongco0@gmail.com>
2024-10-21 20:13:18 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace Group10_Car_Rent
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            btncar1 = new Button();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            btnterms = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            SuspendLayout();
+            // 
+            // btncar1
+            // 
+            btncar1.BackColor = Color.DimGray;
+            btncar1.BackgroundImageLayout = ImageLayout.Zoom;
+            btncar1.Cursor = Cursors.NoMove2D;
+            btncar1.ForeColor = Color.Black;
+            btncar1.Image = (Image)resources.GetObject("btncar1.Image");
+            btncar1.Location = new Point(158, 183);
+            btncar1.Name = "btncar1";
+            btncar1.Size = new Size(289, 177);
+            btncar1.TabIndex = 1;
+            btncar1.UseVisualStyleBackColor = false;
+            btncar1.Click += button1_Click;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.DimGray;
+            button1.BackgroundImageLayout = ImageLayout.Zoom;
+            button1.Cursor = Cursors.NoMove2D;
+            button1.ForeColor = Color.Black;
+            button1.Image = (Image)resources.GetObject("button1.Image");
+            button1.Location = new Point(518, 183);
+            button1.Name = "button1";
+            button1.Size = new Size(289, 177);
+            button1.TabIndex = 2;
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click_1;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.DimGray;
+            button2.BackgroundImageLayout = ImageLayout.Zoom;
+            button2.Cursor = Cursors.NoMove2D;
+            button2.ForeColor = Color.Black;
+            button2.Image = (Image)resources.GetObject("button2.Image");
+            button2.Location = new Point(874, 183);
+            button2.Name = "button2";
+            button2.Size = new Size(289, 177);
+            button2.TabIndex = 3;
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.DimGray;
+            button3.BackgroundImageLayout = ImageLayout.Zoom;
+            button3.Cursor = Cursors.NoMove2D;
+            button3.ForeColor = Color.Black;
+            button3.Image = (Image)resources.GetObject("button3.Image");
+            button3.Location = new Point(158, 494);
+            button3.Name = "button3";
+            button3.Size = new Size(289, 177);
+            button3.TabIndex = 4;
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.DimGray;
+            button4.BackgroundImageLayout = ImageLayout.Zoom;
+            button4.Cursor = Cursors.NoMove2D;
+            button4.ForeColor = Color.Black;
+            button4.Image = (Image)resources.GetObject("button4.Image");
+            button4.Location = new Point(518, 494);
+            button4.Name = "button4";
+            button4.Size = new Size(289, 177);
+            button4.TabIndex = 5;
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            button5.BackColor = Color.DimGray;
+            button5.BackgroundImageLayout = ImageLayout.Zoom;
+            button5.Cursor = Cursors.NoMove2D;
+            button5.ForeColor = Color.Black;
+            button5.Image = (Image)resources.GetObject("button5.Image");
+            button5.Location = new Point(874, 494);
+            button5.Name = "button5";
+            button5.Size = new Size(289, 177);
+            button5.TabIndex = 6;
+            button5.UseVisualStyleBackColor = false;
+            // 
+            // btnterms
+            // 
+            btnterms.BackColor = Color.White;
+            btnterms.Font = new Font("Sitka Heading", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnterms.Location = new Point(897, 3);
+            btnterms.Name = "btnterms";
+            btnterms.Size = new Size(133, 34);
+            btnterms.TabIndex = 7;
+            btnterms.Text = "Terms and Conditions";
+            btnterms.UseVisualStyleBackColor = false;
+            // 
+            // button7
+            // 
+            button7.BackColor = Color.White;
+            button7.Font = new Font("Sitka Heading", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button7.Location = new Point(1046, 3);
+            button7.Name = "button7";
+            button7.Size = new Size(99, 34);
+            button7.TabIndex = 8;
+            button7.Text = "About Us";
+            button7.UseVisualStyleBackColor = false;
+            // 
+            // button8
+            // 
+            button8.BackColor = Color.White;
+            button8.Font = new Font("Sitka Heading", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button8.Location = new Point(1159, 3);
+            button8.Name = "button8";
+            button8.Size = new Size(108, 34);
+            button8.TabIndex = 9;
+            button8.Text = "Admin";
+            button8.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            label1.BackColor = Color.Transparent;
+            label1.BorderStyle = BorderStyle.FixedSingle;
+            label1.Font = new Font("Sitka Heading", 26.2499962F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(481, 108);
+            label1.Name = "label1";
+            label1.Size = new Size(364, 56);
+            label1.TabIndex = 10;
+            label1.Text = "Two Seater Cars";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            label2.BackColor = Color.Transparent;
+            label2.BorderStyle = BorderStyle.FixedSingle;
+            label2.Font = new Font("Sitka Heading", 26.2499962F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(481, 423);
+            label2.Name = "label2";
+            label2.Size = new Size(364, 56);
+            label2.TabIndex = 11;
+            label2.Text = "Family Cars";
+            label2.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            ClientSize = new Size(1279, 752);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(btnterms);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(btncar1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private Button btncar1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button btnterms;
+        private Button button7;
+        private Button button8;
+        private Label label1;
+        private Label label2;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,20 @@
+namespace Group10_Car_Rent
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
